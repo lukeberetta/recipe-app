83a654fbd77e116fc3be5fd1576d820e7d77535b
Luke Beretta <lukeberetta@SAMAC072.local>
2020-11-14 12:18:43 +0200

    potential fuckup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
+import styled from "styled-components";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Recipes } from "./components/Recipes";
 
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
+      <Body>
+        <Recipes />
+      </Body>
       <Navbar />
-      <Recipes />
-    </div>
+      <Footer />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+`;
+
+const Body = styled.div`
+  height: calc(100% - 1px);
+  width: 100%;
+  margin-top: 60px;
+  z-index: 1;
+  overflow-y: scroll;
+  -webkit-overflow-scrolling: touch;
+`;
+
+const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 40px;
+  background: #cccccc;
+  z-index: 11;
+`;
