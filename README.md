c6514c83e77e36fababab6872e59d3fee7804050
Luke Beretta <lukeberetta@SAMAC072.local>
2020-11-19 07:45:52 +0200

    maintenance

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Recepie PWA
+# Tess's Kitchen
+
+A progressive web app for creating and managing beautiful homemade recipes.
+
+### Screenshots:
+
+<img src="./public/img/screenshots.png">
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,7 @@
 import firebase from "firebase";
-
-const config = {
-  apiKey: "AIzaSyAfkLCNsVuMKdnxwbgMMERaLpOgrnTyOHQ",
-  authDomain: "bug-recipe-pwa.firebaseapp.com",
-  databaseURL: "https://bug-recipe-pwa.firebaseio.com",
-  projectId: "bug-recipe-pwa",
-  storageBucket: "bug-recipe-pwa.appspot.com",
-  messagingSenderId: "429142203999",
-  appId: "1:429142203999:web:849ecc4d5114a3d84b45ea",
-};
+import { firebaseConfig } from "./firebase.config";
 
 // Initialize Firebase
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
