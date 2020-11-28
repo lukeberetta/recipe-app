aecf345e8733159e886408ed82760e74f143fc26
Luke Beretta <lukeberetta@SAMAC072.local>
2020-11-28 10:04:19 +0200

    add list to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import {
   Toolbar,
   withTheme,
 } from "@material-ui/core";
-import { Add } from "@material-ui/icons";
+import { Add, ShoppingBasketOutlined } from "@material-ui/icons";
 import { useState } from "react";
 import styled from "styled-components";
 import { Logo } from "./Logo";
@@ -13,9 +13,14 @@ import { NewRecipeForm } from "./NewRecipeForm";
 import { Toast } from "./Toast";
 
 function Navbar() {
-  const [open, setOpen] = useState(false);
-  const handleDrawer = () => {
-    setOpen(!open);
+  const [openForm, setOpenForm] = useState(false);
+  const handleForm = () => {
+    setOpenForm(!openForm);
+  };
+
+  const [openList, setOpenList] = useState(false);
+  const handleList = () => {
+    setOpenList(!openList);
   };
 
   const [openToast, setOpenToast] = useState(false);
@@ -27,14 +32,20 @@ function Navbar() {
     <>
       <Nav color="secondary" position="sticky">
         <Container>
-          <AddButton onClick={handleDrawer}>
+          <NewRecipeButton onClick={handleForm}>
             <Add />
-          </AddButton>
+          </NewRecipeButton>
           <Logo />
+          <ShoppingListButton onClick={handleList}>
+            <ShoppingBasketOutlined />
+          </ShoppingListButton>
         </Container>
       </Nav>
-      <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
-        <NewRecipeForm handleDrawer={handleDrawer} handleToast={handleToast} />
+      <Drawer anchor="right" open={openForm} onClose={() => setOpenForm(false)}>
+        <NewRecipeForm handleDrawer={handleForm} handleToast={handleToast} />
+      </Drawer>
+      <Drawer anchor="left" open={openList} onClose={() => setOpenList(false)}>
+        <div style={{ width: "80vw" }}>Shopping list</div>
       </Drawer>
       <Toast open={openToast} close={handleToast} message="Recipe Added 🥳" />
     </>
@@ -51,10 +62,16 @@ const Container = styled(Toolbar)`
   justify-content: center;
 `;
 
-const AddButton = withTheme(styled(IconButton)`
+const NewRecipeButton = withTheme(styled(IconButton)`
   position: absolute;
   transform: scale(0.8);
   right: 16px;
   background: ${(props) => props.theme.palette.primary.main};
   color: white;
+`);
+
+const ShoppingListButton = withTheme(styled(IconButton)`
+  position: absolute;
+  left: 16px;
+  color: ${(props) => props.theme.palette.primary.main};
 `);
